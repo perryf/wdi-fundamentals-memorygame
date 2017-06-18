e56d040d1e6c739decdb6a5a332853b36e86c44e
perryf <pjfust@gmail.com>
2017-06-18 12:25:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -17,7 +17,7 @@ var cards = [
 	{
 		rank: "king",
 		suit: "diamonds",
-		cardImage: "images/queen-of-diamonds.png"
+		cardImage: "images/king-of-diamonds.png"
 	}
 ];
 
@@ -29,17 +29,29 @@ var checkForMatch = function() {
 			alert("You found a match!");
 		} else {
 			alert("Sorry try again.");
+			this.setAttribute('src', 'images/back.png');
 		}
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', [i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
